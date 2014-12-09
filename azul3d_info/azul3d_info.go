b3f6770b684bd44066828f4e69c563192041e06e
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-09 07:40:37 -0700

    azul3d_info: Renderer -> Device

--- azul3d_info/azul3d_info.go
+++ azul3d_info/azul3d_info.go
@@ -13,42 +13,42 @@ import (
 )
 
 // gfxLoop is responsible for drawing things to the window.
-func gfxLoop(w window.Window, r gfx.Renderer) {
+func gfxLoop(w window.Window, d gfx.Device) {
 	defer w.Close()
 
-	gpu := r.GPUInfo()
-	fmt.Println("GPU Name:", gpu.Name)
-	fmt.Println("GPU Vendor:", gpu.Vendor)
-	fmt.Printf("OpenGL: v%d.%d\n", gpu.GLMajor, gpu.GLMinor)
-	fmt.Printf("GLSL: v%d.%d\n", gpu.GLSLMajor, gpu.GLSLMinor)
-	fmt.Println("OcclusionQuery =", gpu.OcclusionQuery)
-	fmt.Println("OcclusionQueryBits =", gpu.OcclusionQueryBits)
-	fmt.Println("MaxTextureSize =", gpu.MaxTextureSize)
-	fmt.Println("NPOT Textures =", gpu.NPOT)
+	dev := d.Info()
+	fmt.Println("Device Name:", dev.Name)
+	fmt.Println("Device Vendor:", dev.Vendor)
+	fmt.Printf("OpenGL: v%d.%d\n", dev.GLMajor, dev.GLMinor)
+	fmt.Printf("GLSL: v%d.%d\n", dev.GLSLMajor, dev.GLSLMinor)
+	fmt.Println("OcclusionQuery =", dev.OcclusionQuery)
+	fmt.Println("OcclusionQueryBits =", dev.OcclusionQueryBits)
+	fmt.Println("MaxTextureSize =", dev.MaxTextureSize)
+	fmt.Println("NPOT Textures =", dev.NPOT)
 
-	fmt.Printf("%d Render-To-Texture MSAA Formats:\n", len(gpu.RTTFormats.Samples))
-	for i, sampleCount := range gpu.RTTFormats.Samples {
+	fmt.Printf("%d Render-To-Texture MSAA Formats:\n", len(dev.RTTFormats.Samples))
+	for i, sampleCount := range dev.RTTFormats.Samples {
 		fmt.Printf("    %d. %dx MSAA\n", i+1, sampleCount)
 	}
 
-	fmt.Printf("%d Render-To-Texture Color Formats:\n", len(gpu.RTTFormats.ColorFormats))
-	for i, f := range gpu.RTTFormats.ColorFormats {
+	fmt.Printf("%d Render-To-Texture Color Formats:\n", len(dev.RTTFormats.ColorFormats))
+	for i, f := range dev.RTTFormats.ColorFormats {
 		fmt.Printf("    %d. %+v\n", i+1, f)
 	}
-	fmt.Printf("%d Render-To-Texture Depth Formats:\n", len(gpu.RTTFormats.DepthFormats))
-	for i, f := range gpu.RTTFormats.DepthFormats {
+	fmt.Printf("%d Render-To-Texture Depth Formats:\n", len(dev.RTTFormats.DepthFormats))
+	for i, f := range dev.RTTFormats.DepthFormats {
 		fmt.Printf("    %d. %+v\n", i+1, f)
 	}
-	fmt.Printf("%d Render-To-Texture Stencil Formats:\n", len(gpu.RTTFormats.StencilFormats))
-	for i, f := range gpu.RTTFormats.StencilFormats {
+	fmt.Printf("%d Render-To-Texture Stencil Formats:\n", len(dev.RTTFormats.StencilFormats))
+	for i, f := range dev.RTTFormats.StencilFormats {
 		fmt.Printf("    %d. %+v\n", i+1, f)
 	}
 
-	fmt.Println("AlphaToCoverage =", gpu.AlphaToCoverage)
-	fmt.Println("GLSLMaxVaryingFloats =", gpu.GLSLMaxVaryingFloats)
-	fmt.Println("GLSLMaxVertexInputs =", gpu.GLSLMaxVertexInputs)
-	fmt.Println("GLSLMaxFragmentInputs =", gpu.GLSLMaxFragmentInputs)
-	fmt.Println("OpenGL extensions:", gpu.GLExtensions)
+	fmt.Println("AlphaToCoverage =", dev.AlphaToCoverage)
+	fmt.Println("GLSLMaxVaryingFloats =", dev.GLSLMaxVaryingFloats)
+	fmt.Println("GLSLMaxVertexInputs =", dev.GLSLMaxVertexInputs)
+	fmt.Println("GLSLMaxFragmentInputs =", dev.GLSLMaxFragmentInputs)
+	fmt.Println("OpenGL extensions:", dev.GLExtensions)
 }
 
 func main() {
