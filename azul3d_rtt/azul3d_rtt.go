15cfc67b9cb386f96f6f1575a3fe743ada78f2ed
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-11 01:36:57 -0700

    azul3d_rtt: utilize the seperate GLSLShader struct; See azul3d/gfx#63

--- azul3d_rtt/azul3d_rtt.go
+++ azul3d_rtt/azul3d_rtt.go
@@ -96,8 +96,10 @@ func gfxLoop(w window.Window, d gfx.Device) {
 
 	// Create a simple shader.
 	shader := gfx.NewShader("SimpleShader")
-	shader.GLSLVert = glslVert
-	shader.GLSLFrag = glslFrag
+	shader.GLSL = &gfx.GLSLShader{
+		Vertex: glslVert,
+		Fragment: glslFrag,
+	}
 
 	// Create a card mesh.
 	cardMesh := gfx.NewMesh()
